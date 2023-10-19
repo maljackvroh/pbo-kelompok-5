d754c3c39ba03dd0464a7b2d444e55d210358ed3
notforsilent <notforsilent@gmail.com>
2023-10-19 17:16:51 +0800

    terubah:         P1nomor5_F1B021104.java

--- P1nomor5_F1B021104.java
+++ P1nomor5_F1B021104.java
@@ -1,38 +1,61 @@
-import java.util.Scanner;
+class Mobil {
+    String merek;
+    String model;
+    String warna;
+    String bahanBakar;
+    int harga;
 
-public class P1nomor5_F1B021104 {
-
-    public P1nomor5_F1B021104() {
-        String merek;
-        String model;
-        String warna;
-        String bahanBakar;
-        int harga;
-        int speedd;
-
+    public Mobil (String a, String b, String c, String d, int e){
+        merek = a;
+        model = b;
+        warna = c;
+        bahanBakar = d;
+        harga = e;
+    }
+    
+    public void display() {
+        System.out.println(merek);
+        System.out.println(model);
+        System.out.println(warna);
+        System.out.println(bahanBakar);
+        System.out.println(harga);
+        
     }
 
+}
+
+class Motor {
+    String merek;
+    String model;
+    String warna;
+    String bahanBakar;
+    int harga;
+
+    public Motor (String a, String b, String c, String d, int e){
+        merek = a;
+        model = b;
+        warna = c;
+        bahanBakar = d;
+        harga = e;
+    }
+    
+    public void display() {
+        System.out.println(merek);
+        System.out.println(model);
+        System.out.println(warna);
+        System.out.println(bahanBakar);
+        System.out.println(harga);
+        
+        
+    }
+}
+
+public class P1nomor5_F1B021104{
     public static void main(String[] args) {
-        P1nomor5_F1B021104 newMobil = new P1nomor5_F1B021104();
-        Scanner obj = new Scanner(System.in);
+        Mobil mobill = new Mobil("Toyota", "Van", "Black mate", "Pertamax", 123985019);
+        Motor motorr = new Motor("Kawasaki", "CB 150", "Black Mamba", "Pertamax Turbo", 50000000);
 
-        System.out.println("Masukan data Mobil: ");
-        newMobil.merek = obj.nextLine();
-        newMobil.model = obj.nextLine();
-        newMobil.warna = obj.nextLine();
-        newMobil.bahanBakar = obj.nextLine();
-        newMobil.harga = obj.nextInt();
-        newMobil.speedd = obj.nextInt();
-
-        System.out.println("\nBerikut data mobil yang anda masukan: ");
-        System.out.println("Merek"+newMobil.merek);
-        System.out.println("Model: "+newMobil.model);
-        System.out.println("Warna: "+newMobil.warna);
-        System.out.println("Bahan bakar: "+newMobil.bahanBakar);
-        System.out.println("harga: "+newMobil.harga);
-        System.out.println("speed: "+newMobil.speedd);
-
-
-        newMobil.speed(newMobil.speedd);
+        mobill.display();
+        motorr.display();
     }
 }
