217ecc5fa5cb23114dda565926db9c4f11d534af
notforsilent <notforsilent@gmail.com>
2023-10-20 10:55:39 +0800

    terubah:         percobaan_2/js4.java

--- percobaan_2/js4.java
+++ percobaan_2/js4.java
@@ -1,5 +1,37 @@
 package percobaan_2;
 
+class Data_diri {
+    String nama;
+    int umur;
+    String alamat;
+    String hoby;
+
+    void display1(){
+        System.out.println("\nNama\t: "+nama);
+    }
+    void display2(){
+        System.out.println("Umur\t: "+umur);
+    }
+    void display3(){
+        System.out.println("Alamat\t: "+alamat);
+    }
+    void display4(){
+        System.out.println("Hoby\t: "+hoby);
+    }
+}
+
 public class js4 {
-    
+    public static void main(String[] args) {
+        Data_diri bio = new Data_diri();
+
+        bio.nama = "Akmal Dzaki";
+        bio.umur = 21;
+        bio.alamat = "Jln Melur No 1.A";
+        bio.hoby = "Browsing";
+
+        bio.display1();
+        bio.display2();
+        bio.display3();
+        bio.display4();
+    }
 }
