436d51c90d1d36ae9e7093af54f3e100fbbfa9cd
notforsilent <notforsilent@gmail.com>
2023-10-20 10:22:43 +0800

    berkas baru:     Tugas1_AkmalDzaki_F1B021104.java 	berkas baru:     percobaan_2/js1.java 	berkas baru:     percobaan_2/js2.java

---
+++ Tugas1_AkmalDzaki_F1B021104.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class data {
+    int data;
+    int dataAkhir;
+    int dataAwal;
+    
+}
+
+public class Tugas1_AkmalDzaki_F1B021104 {
+    public static void main(String[] args) {
+        data num = new data();
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("PROGRAM PENGECEKAN BILANGAN POLINDROM ANGKA");
+        System.out.println("masukan bilangan :");
+
+        num.data = obj.nextInt();
+
+
+    }
+}
---
+++ percobaan_2/js1.java
@@ -0,0 +1,26 @@
+package percobaan_2;
+
+class Mahasiswa {
+    String nama;
+    String nim;
+
+    void display1 () {
+        System.out.println("\nNama\t: "+nama);
+    }
+
+    void display2(){
+        System.out.println("NIM\t: "+nim);
+    }
+}
+
+public class js1 {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+
+        mhs.nama = "Akmal Dzaki";
+        mhs.nim = "F1B021104";
+
+        mhs.display1();
+        mhs.display2();
+    }
+}
