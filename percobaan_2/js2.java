023056f4b6858690878daf06ebf5843a84881c7a
notforsilent <notforsilent@gmail.com>
2023-10-20 10:32:40 +0800

    terubah:         percobaan_2/js2.java

--- percobaan_2/js2.java
+++ percobaan_2/js2.java
@@ -1,5 +1,32 @@
 package percobaan_2;
 
-public class js2 {
+class Hewan {
+    String[] karnivora = {"Macan","Singa","Buaya"};
+    String[] herbivora = {"Gajah","kambing","Jerapah"};
+    int bnyakData;
+
+    void display1(){
+        System.out.println("\nMacam macam hewan karnivora: ");
+        for(int i = 0; i < karnivora.length; i++){
+            System.out.println(i+". "+karnivora[i]);
+        }
+    }
+
+    void display2(){
+        System.out.println("\nMacam macam hewan herbivora: ");
+        for(int i = 0; i < herbivora.length; i++){
+            System.out.println(i+". "+herbivora[i]);
+        }
+    }
     
+}
+
+public class js2 {
+    public static void main(String[] args) {
+        Hewan hewan = new Hewan();
+   
+
+        hewan.display1();
+        hewan.display2();
+    }
 }
