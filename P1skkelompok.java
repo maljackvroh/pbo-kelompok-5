f6607943746ea9b0c5284d96d1e2e736bae808ab
notforsilent <notforsilent@gmail.com>
2023-10-29 13:37:49 +0800

    berkas baru:     P1skkelompok.java

---
+++ P1skkelompok.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+class Cars {
+    String merek;
+    String model;
+    String warna;
+    String bahanBakar;
+    int harga;
+    int speedd;
+
+    public void display(String merek, String model, String warna, String bahanBakar, int harga){
+        
+        System.out.println("\nData Mobil 1:");
+        System.out.println("Merek\t\t: "+merek);
+        System.out.println("Model\t\t: "+model);
+        System.out.println("Warna\t\t: "+warna);
+        System.out.println("Bahan Bakar\t: "+bahanBakar);
+        System.out.println("harga\t\t: "+harga);
+    }
+
+    public void speed(int speedd){
+        System.out.println("Max speed is: " +speedd);
+    }
+}
+
+public class P1skkelompok {
+    public static void main(String[] args) {
+        Cars newCars = new Cars();
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("Masukan data Mobil");
+        System.out.println("Merek: ");
+        newCars.merek = obj.nextLine();
+        System.out.println("Model: ");
+        newCars.model = obj.nextLine();
+        System.out.println("Warna: ");
+        newCars.warna = obj.nextLine();
+        System.out.println("Bahan Bakar: ");
+        newCars.bahanBakar = obj.nextLine();
+        System.out.println("Harga(nominal saja): ");
+        newCars.harga = obj.nextInt();
+        System.out.println("Max Speed: ");
+        newCars.speedd = obj.nextInt();
+
+        System.out.println("\nBerikut data Mobil yang anda masukan: ");
+        System.out.println("Merek"+newCars.merek);
+        System.out.println("Model: "+newCars.model);
+        System.out.println("Warna: "+newCars.warna);
+        System.out.println("Bahan bakar: "+newCars.bahanBakar);
+        System.out.println("harga: "+newCars.harga);
+        System.out.println("speed: "+newCars.speedd);
+
+
+        newCars.speed(newCars.speedd);
+    }
+}
